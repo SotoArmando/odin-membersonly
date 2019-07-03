9fc0e6c58edb5584fcd7d2942ae367ed0c4189da
ispirett <ispirett@gmail.com>
2019-07-03 18:50:42 -0400

    feature(post): added some auth funtions ad some design also fixed some issues with the auth system

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,23 @@
 class PostsController < ApplicationController
-  before_action :logged_in_user,     only: [:new,:create]
+  before_action :logged_in_user, only: [:create]
+
+  def index
+    @posts = Post.all
+  end
+  def new
+    @post = Post.new
+  end
   def create
       @post = Post.new
       @post.user_id = current_user.id 
-      @post.name = params.require(:post).permit(:name)
-      @post.desc = params.require(:post).permit(:desc)
+      @post.name = params.require(:post).permit(:title)
+      @post.desc = params.require(:post).permit(:body)
       @post.save
   end
-  def new 
-      @post = Post.new
-  end
+
   def edit 
   end
+
   private 
       def logged_in_user
           unless logged_in?
