1a7fec18a9e861f0c3ab435a6f062317d14c9a5c
Macatuz <armando29@live.com>
2019-07-04 10:48:05 -0400

    + fix redirec_to '/sign_in'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,7 +22,7 @@ class PostsController < ApplicationController
       def logged_in_user
           unless logged_in?
           flash[:danger] = "Please log in."
-          redirect_to '/sign_up'
+          redirect_to '/sign_in'
           end 
       end
 
