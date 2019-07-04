6b27bb3014bbd6ae160be2d492f628eba49141b8
Macatuz <armando29@live.com>
2019-07-04 12:15:20 -0400

    Added Sign up Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  get '/sign_in', to:'sessions#new'
+  get '/sign_in', to:'sessions#new' 
+  get '/sign_in', to:'sessions#new' ,as: 'users_path'
   get '/index', to:'sessions#edit'
+  get '/sign_up', to:'users#new'
+  post '/sign_up', to:'users#create'
   post '/sign_in' ,to:'sessions#create' , as: 'sign_in_path'
   delete '/sign_out' ,to:'sessions#destroy'
-  resources :posts
 
+  get '/users', to:'sessions#new'
+  post '/users', to:'sessions#new'
+  
+  resources :posts
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
