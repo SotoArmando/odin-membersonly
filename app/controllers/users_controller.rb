46ec08396ed9b02a231c99d4cb34ce8ce96f6848
ispirett <ispirett@gmail.com>
2019-07-04 12:28:28 -0400

    feature(sign_up): created sign up  and fixed some errors

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] =  'Account created'
+      log_in(@user)
+      remember(@user)
+      redirect_to posts_path
+    else
+      flash[:danger] = "there are some error please resolve them #{@user.errors.full_messages}"
+    end
+  end
+
+
+
+  private
+  def user_params
+  params.require(:user).permit(:email, :name, :password )
+  end
+end
